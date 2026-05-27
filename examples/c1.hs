de8baf66cf92d5f7e068b13dccc7954e0b6edf5f
JVPVO <jvpvo@users.noreply.github.com>
2026-05-27 15:10:20 +0000

    Add examples for trabalho LP

---
+++ examples/c1.hs
@@ -0,0 +1,5 @@
+module Main where
+
+main = do
+  let x = 10
+  if x > 0 then print x else print 0
---
+++ examples/c2.hs
@@ -0,0 +1,5 @@
+module Main where
+
+main = do
+  let y = 10
+  if y > 0 then print y else print 1
